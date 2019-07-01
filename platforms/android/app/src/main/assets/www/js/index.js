7ce990de2486e6946539077f2e6ce8275a97a3a6
kchrs <costas.christopoulos@gmail.com>
2019-07-01 18:34:30 +0300

    show current user details

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -118,11 +118,31 @@ function facebookSignin() {
     
    //   console.log("FB TOKEN: "+token);
    //    console.log("USER : "+user);
+
+   var user = firebase.auth().currentUser;
+
+if (user != null) {
+  user.providerData.forEach(function (profile) {
+    console.log("Sign-in provider: " + profile.providerId);
+    console.log("  Provider-specific UID: " + profile.uid);
+    console.log("  Name: " + profile.displayName);
+    console.log("  Email: " + profile.email);
+    console.log("  Photo URL: " + profile.photoURL);
+  });
+}
+
+
    }).catch(function(error) {
       console.log("error "+error);
       // console.log(error.message);
    });
 }
+
+
+
+
+
+
 function facebookSignout() {
  firebase.auth().signOut()
  .then(function() {
